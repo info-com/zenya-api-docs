9d65013b7cad826ce5e37a73b7a0ac8b2e9aad64
Jonathan Spalink <jspalink@info.com>
2015-01-27 15:46:17 -0600

    fixed copyright

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'eContext API'
-copyright = u'2014, Jonathan Spalink'
+copyright = u'2015, eContext.com'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
